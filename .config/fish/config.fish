90b010230b90c09d1a14f7c01aabcf132d28c8d3
JD Angerhofer <jd.angerhofer@gmail.com>
2025-01-07 12:57:27 -0500

    Launch `idea` with fuzzy matching via `z`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,6 +58,22 @@ function idea
     disown
 end
 
+function i
+    if test (count $argv) -eq 1
+        # Use `z` to resolve the directory
+        set dir (z -e $argv[1])
+        if test -n "$dir"
+            /Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea "$dir" >/dev/null 2>&1 &
+            disown
+        else
+            echo "No match found for '$argv[1]'"
+        end
+    else
+        echo "Usage: idea <directory>"
+    end
+end
+
+
 ########################################
 # Node
 ########################################
