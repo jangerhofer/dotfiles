4618e83d2c8c9441be0c581a4dcd45d3ebea0a20
JD Angerhofer <jd.angerhofer@gmail.com>
2025-07-17 20:16:44 -0700

    fix(nix): infinite recusion in `nix` setup

--- .config/nix/modules/home.nix
+++ .config/nix/modules/home.nix
@@ -11,10 +11,8 @@
     ./k9s.nix
     ./fish.nix
     ./ssh.nix
+    ./lazygit.nix
   ];
-  # Home Manager needs a bit of information about you and the paths it should manage
-  home.username = config.home.username;
-  home.homeDirectory = config.home.homeDirectory;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
