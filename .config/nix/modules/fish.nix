fea264fe02cfff5bcb0ae40f112ec8b441a8e6ba
JD Angerhofer <jd.angerhofer@gmail.com>
2025-07-28 10:17:39 -0700

    fix: add `homebrew` to $PATH

--- .config/nix/modules/fish.nix
+++ .config/nix/modules/fish.nix
@@ -55,6 +55,11 @@
           set -gx TERM xterm-256color
       end
       
+      # Initialize Homebrew
+      if test -f /opt/homebrew/bin/brew
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+      end
+      
       # Ensure nix profiles are in PATH
       if test -d ~/.nix-profile/bin
           set -gx PATH ~/.nix-profile/bin $PATH
