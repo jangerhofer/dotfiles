035701832efc6d7657cd7054667f4d64bd074e1a
JD Angerhofer <jd.angerhofer@gmail.com>
2025-11-19 14:23:29 -0600

    chore: align nix flake inputs to 25.05 release

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -2,13 +2,13 @@
   description = "Cross-platform development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
