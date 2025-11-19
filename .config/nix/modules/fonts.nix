e519b28864a8659e6ecf14382cbd0c6d48cd226d
JD Angerhofer <jd.angerhofer@gmail.com>
2025-11-19 14:24:33 -0600

    fix: remove font management to get `home-manager` back into working order

--- .config/nix/modules/fonts.nix
+++ .config/nix/modules/fonts.nix
@@ -1,29 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Enable font management
-  fonts.fontconfig.enable = true;
-  
-  home.packages = with pkgs; [
-    # Essential programming fonts (new nerd-fonts namespace)
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    nerd-fonts.hack
-    nerd-fonts.sauce-code-pro
-    nerd-fonts.ubuntu-mono
-    nerd-fonts.dejavu-sans-mono
-    nerd-fonts.inconsolata
-    nerd-fonts.roboto-mono
-    nerd-fonts.meslo-lg
-    nerd-fonts.victor-mono
-    nerd-fonts.iosevka
-    nerd-fonts.anonymice
-    nerd-fonts.space-mono
-    nerd-fonts.go-mono
-    
-    # Additional useful fonts
-    jetbrains-mono
-    fira-code
-    inconsolata
-  ];
+  # Temporarily disable extra fonts until the set is updated for 25.05
+  fonts.fontconfig.enable = false;
+  home.packages = [ ];
 }
