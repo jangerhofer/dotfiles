e08baacbf0ba6433ac43c857153bcea6dcf882ac
JD Angerhofer <jd.angerhofer@gmail.com>
2026-04-24 10:34:29 -0500

    refactor(cloud): prune rarely used CLI tools

--- .config/nix/modules/cloud.nix
+++ .config/nix/modules/cloud.nix
@@ -4,31 +4,21 @@
   home.packages = with pkgs; [
     # Cloud provider CLIs
     google-cloud-sdk # Google Cloud SDK
-    azure-cli       # Azure CLI
-    
+
     # Infrastructure as Code
-    terraform       # Infrastructure provisioning
-    terragrunt      # Terraform wrapper
-    packer          # Image building
-    
+    terraform # Infrastructure provisioning
+    terragrunt # Terraform wrapper
+    packer # Image building
+
     # Container orchestration
-    kubectx        # Kubernetes context switcher
-    
-    # Monitoring and observability
-    prometheus      # Metrics monitoring
-    grafana         # Grafana CLI
-    
-    # Service mesh and networking
-    istioctl        # Istio service mesh CLI
-    linkerd         # Linkerd service mesh CLI
-    
+    kubectx # Kubernetes context switcher
+
     # CI/CD and deployment
-    flyctl          # Fly.io CLI
-    argocd          # Argo CD CLI
-    
+    flyctl # Fly.io CLI
+
     # Development and debugging
-    dive            # Docker image analyzer
-    stern           # Kubernetes log tailing
+    dive # Docker image analyzer
+    stern # Kubernetes log tailing
   ];
 
   # AWS CLI configuration
@@ -45,11 +35,11 @@
   # Set common cloud environment variables
   home.sessionVariables = {
     # AWS
-    AWS_PAGER = "";  # Disable pager for AWS CLI
-    
+    AWS_PAGER = ""; # Disable pager for AWS CLI
+
     # Kubernetes
     KUBECTL_EXTERNAL_DIFF = "delta";
-    
+
     # Terraform
     TF_PLUGIN_CACHE_DIR = "$HOME/.terraform.d/plugin-cache";
   };
