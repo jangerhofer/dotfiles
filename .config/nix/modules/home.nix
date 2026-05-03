e6d1cc6e03bfa9328debad28a9f185065d1322ad
JD Angerhofer <jd.angerhofer@gmail.com>
2026-05-02 21:47:32 -0500

    Fix Home Manager profile add activation

--- .config/nix/modules/home.nix
+++ .config/nix/modules/home.nix
@@ -171,6 +171,48 @@ in
   # Let Home Manager install and manage itself
   programs.home-manager.enable = true;
 
+  # FIXME(upstream-home-manager): remove this override after Home Manager's
+  # `installPackages` activation block switches from `nix profile install` to
+  # `nix profile add`.
+  home.activation.installPackages = lib.mkForce (
+    lib.hm.dag.entryAfter [ "writeBoundary" ] (
+      if config.submoduleSupport.externalPackageInstall then
+        ''
+          nixProfileRemove home-manager-path
+        ''
+      else
+        ''
+          function nixReplaceProfile() {
+            local oldNix="$(command -v nix)"
+
+            nixProfileRemove 'home-manager-path'
+
+            run $oldNix profile add $1
+          }
+
+          if [[ -e ${config.home.profileDirectory}/manifest.json ]] ; then
+            INSTALL_CMD="nix profile add"
+            INSTALL_CMD_ACTUAL="nixReplaceProfile"
+            LIST_CMD="nix profile list"
+            REMOVE_CMD_SYNTAX='nix profile remove {number | store path}'
+          else
+            INSTALL_CMD="nix-env -i"
+            INSTALL_CMD_ACTUAL="run nix-env -i"
+            LIST_CMD="nix-env -q"
+            REMOVE_CMD_SYNTAX='nix-env -e {package name}'
+          fi
+
+          if ! $INSTALL_CMD_ACTUAL ${config.home.path} ; then
+            echo
+            _iError $'Oops, Nix failed to install your new Home Manager profile!\n\nPerhaps there is a conflict with a package that was installed using\n"%s"? Try running\n\n    %s\n\nand if there is a conflicting package you can remove it with\n\n    %s\n\nThen try activating your Home Manager configuration again.' "$INSTALL_CMD" "$LIST_CMD" "$REMOVE_CMD_SYNTAX"
+            exit 1
+          fi
+          unset -f nixReplaceProfile
+          unset INSTALL_CMD INSTALL_CMD_ACTUAL LIST_CMD REMOVE_CMD_SYNTAX
+        ''
+    )
+  );
+
   # Avoid noisy options.json generation on 25.11; use `nnews` to inspect news on demand.
   manual.manpages.enable = false;
   news.display = "silent";
